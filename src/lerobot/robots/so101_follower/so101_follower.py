483f24e0d0abfa5feab49b880153dcc773b1e75f
ankithreddypati <pati.a@northeastern.edu>
2025-07-20 14:57:27 -0400

    robots teleoperate connection retry and dataset recording resume error fix

--- src/lerobot/robots/so101_follower/so101_follower.py
+++ src/lerobot/robots/so101_follower/so101_follower.py
@@ -156,7 +156,7 @@ class SO101Follower(Robot):
 
         # Read arm position
         start = time.perf_counter()
-        obs_dict = self.bus.sync_read("Present_Position")
+        obs_dict = self.bus.sync_read("Present_Position", num_retry=4)
         obs_dict = {f"{motor}.pos": val for motor, val in obs_dict.items()}
         dt_ms = (time.perf_counter() - start) * 1e3
         logger.debug(f"{self} read state: {dt_ms:.1f}ms")
@@ -191,7 +191,7 @@ class SO101Follower(Robot):
         # Cap goal position when too far away from present position.
         # /!\ Slower fps expected due to reading from the follower.
         if self.config.max_relative_target is not None:
-            present_pos = self.bus.sync_read("Present_Position")
+            present_pos = self.bus.sync_read("Present_Position", num_retry=4)
             goal_present_pos = {key: (g_pos, present_pos[key]) for key, g_pos in goal_pos.items()}
             goal_pos = ensure_safe_goal_position(goal_present_pos, self.config.max_relative_target)
 
